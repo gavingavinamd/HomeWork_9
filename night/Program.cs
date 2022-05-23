e5634efcdd9f540df8caf3b6908f44f767adfb12
GavinChou <gavingavinamd@gmail.com>
2022-05-23 16:00:46 +0800

    在 local master 修改 5

--- night/Program.cs
+++ night/Program.cs
@@ -6,3 +6,4 @@ Console.WriteLine("在 GitHub 修改模擬別人 Push 1");
 Console.WriteLine("在 GitHub 修改模擬別人 Push 2");
 Console.WriteLine("在 local master 修改 3 暫時不Push");
 Console.WriteLine("在 local master 修改 4 暫時不Push");
+Console.WriteLine("在 local master 修改 5");
