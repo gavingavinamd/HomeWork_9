8594b629db7d3491a517ec8179ec15e6cf9152b3
GavinChou <gavingavinamd@gmail.com>
2022-05-23 16:08:36 +0800

    在feature分支修改1

--- night/Program.cs
+++ night/Program.cs
@@ -7,3 +7,6 @@ Console.WriteLine("在 GitHub 修改模擬別人 Push 2");
 Console.WriteLine("在 local master 修改 3 暫時不Push");
 Console.WriteLine("在 local master 修改 4 暫時不Push");
 Console.WriteLine("在 local master 修改 5");
+Console.WriteLine("在 feature 分支修改 1");
+
+
